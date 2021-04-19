9b23215fea86758e3a5dd1f5a116a0cd69258cbb
Amol Dogra <amol88658@gmail.com>
2021-04-19 23:50:15 +0530

    Employee Created

--- app/src/main/java/com/SB/shreeboutique/activity/Employees.kt
+++ app/src/main/java/com/SB/shreeboutique/activity/Employees.kt
@@ -3,10 +3,14 @@ package com.SB.shreeboutique.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.SB.shreeboutique.R
+import com.SB.shreeboutique.fragm.EmployeeFragment
+import com.SB.shreeboutique.fragment.BookingsFragment
 
 class Employees : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_employees)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.employee_container, EmployeeFragment()).commit()
     }
 }
