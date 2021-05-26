de0775fd275aeebbb6642260ae68b78e3830fc1d
Jai Shiv <nsoni19012001@gmail.com>
2021-05-26 18:11:39 +0530

    ADD: Recycler View and Delete on Swipe

---
+++ app/src/main/java/com/SB/shreeboutique/ui/adapters/EmployeesAdapter.kt
@@ -0,0 +1,60 @@
+package com.SB.shreeboutique.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.SB.shreeboutique.R
+import com.SB.shreeboutique.db.Employee
+
+class EmployeesAdapter : RecyclerView.Adapter<EmployeesAdapter.EmployeeViewHolder>() {
+    class EmployeeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tv_employeeName: TextView = view.findViewById(R.id.tv_employeeName)
+        val tv_position: TextView = view.findViewById(R.id.tv_position)
+        val tv_employeeSalary: TextView = view.findViewById(R.id.tv_employeeSalary)
+        val tv_employeeDate: TextView = view.findViewById(R.id.tv_employeeDate)
+    }
+
+    private val differCallback = object : DiffUtil.ItemCallback<Employee>() {
+        override fun areItemsTheSame(oldItem: Employee, newItem: Employee): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Employee, newItem: Employee): Boolean {
+            return oldItem == newItem
+        }
+    }
+    val differ = AsyncListDiffer(this, differCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EmployeeViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.employee_single_layout, parent, false)
+        return EmployeeViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: EmployeeViewHolder, position: Int) {
+        val employee = differ.currentList[position]
+        holder.itemView.apply {
+            holder.tv_employeeName.text = employee.name
+            holder.tv_position.text = employee.position
+            holder.tv_employeeSalary.text = employee.salary.toString()
+            holder.tv_employeeDate.text = employee.dateOfJoining
+        }
+        setOnItemClickListener {
+            onItemClickListener?.let { it(employee) }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    private var onItemClickListener: ((Employee) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (Employee) -> Unit) {
+        onItemClickListener = listener
+    }
+}
--- app/src/main/java/com/SB/shreeboutique/ui/fragment/EmployeeFragment.kt
+++ app/src/main/java/com/SB/shreeboutique/ui/fragment/EmployeeFragment.kt
@@ -1,32 +1,88 @@
 package com.SB.shreeboutique.fragm
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.SB.shreeboutique.R
+import com.SB.shreeboutique.ui.activity.Employees
+import com.SB.shreeboutique.ui.adapters.EmployeesAdapter
 import com.SB.shreeboutique.ui.fragment.NewEmployeeFragment
+import com.SB.shreeboutique.ui.viewmodels.MainViewModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class EmployeeFragment : Fragment(R.layout.fragment_employee) {
-    lateinit var fabEmployee : FloatingActionButton
+    lateinit var fabEmployee: FloatingActionButton
+    lateinit var rv_employee: RecyclerView
+    lateinit var employeesAdapter: EmployeesAdapter
+    lateinit var viewModel: MainViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_employee,container,false)
+        val view = inflater.inflate(R.layout.fragment_employee, container, false)
         initialization(view)
+
+        rv_employee.apply {
+            adapter = employeesAdapter
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        }
+
+        viewModel.getAllEmployees().observe(viewLifecycleOwner, Observer { employee ->
+            employeesAdapter.differ.submitList(employee)
+        })
+
+        val itemTouchHelper = object :
+            ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val employee = employeesAdapter.differ.currentList[position]
+                viewModel.delete(employee)
+
+                Snackbar.make(
+                    rv_employee,
+                    "${employee.name} Deleted Successfully!!",
+                    Snackbar.LENGTH_LONG
+                ).setAction("Undo", { viewModel.insert(employee) }).show()
+
+            }
+        }
+
+        ItemTouchHelper(itemTouchHelper).attachToRecyclerView(rv_employee)
 
         fabEmployee.setOnClickListener {
             parentFragmentManager.beginTransaction()
-                .replace(R.id.employee_container,  NewEmployeeFragment()).commit()
+                .replace(R.id.employee_container, NewEmployeeFragment()).commit()
         }
         return view
 
     }
-    private fun initialization(view: View){
+
+    private fun initialization(view: View) {
+        viewModel = (activity as Employees).viewModel
+        employeesAdapter = EmployeesAdapter()
         fabEmployee = view.findViewById(R.id.fab_add_employee)
+        rv_employee = view.findViewById(R.id.rv_employee)
     }
 }
